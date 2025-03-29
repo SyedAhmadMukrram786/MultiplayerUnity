df621ca1b3f2e14dd7a0928db36e2e3814e6ba0e
syedahmadmukrram13@gmail.com <syedahmadmukrram13@gmail.com>
2025-03-29 15:16:19 +0500

    Worked On RPC(Server and Client)

--- Assets/Scripts/PlayerNetwork.cs
+++ Assets/Scripts/PlayerNetwork.cs
@@ -1,27 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using Unity.Netcode;
 using UnityEngine;
 
 public class PlayerNetwork : NetworkBehaviour
 {
+    [SerializeField] private Transform spawnedObjectPrefab;
+    private Transform spawnedObjectTransform;
 
-    private NetworkVariable<int> RandomNumber = new NetworkVariable<int>(1, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+    private NetworkVariable<MyCustomData> RandomNumber = new NetworkVariable<MyCustomData>(
+        new MyCustomData
+        {
+            _int = 59,
+            _bool = true,
+        }, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+
+    [System.Serializable]
+    public struct MyCustomData : INetworkSerializable
+    {
+        public int _int;
+        public bool _bool;
+        public string message;
+
+        public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+        {
+            serializer.SerializeValue(ref _int);
+            serializer.SerializeValue(ref _bool);
+            serializer.SerializeValue(ref message);
+        }
+    }
 
     public override void OnNetworkSpawn()
     {
-        RandomNumber.OnValueChanged += (int previouseValue, int newValue) =>
+        RandomNumber.OnValueChanged += (MyCustomData previouseValue, MyCustomData newValue) =>
         {
-            Debug.Log(OwnerClientId + " Random Number: " + RandomNumber.Value);
+            Debug.Log(OwnerClientId + "; " + newValue._int + "; " + newValue._bool + "; " + newValue.message);
         };
     }
+
     private void Update()
     {
         if (!IsOwner) return;
-    
+
         if (Input.GetKeyDown(KeyCode.T))
         {
-            RandomNumber.Value = Random.Range(0, 100);
+            spawnedObjectTransform = Instantiate(spawnedObjectPrefab);
+            spawnedObjectTransform.GetComponent<NetworkObject>().Spawn(true);
+            //TestServerRpc(new ServerRpcParams());
+            //TestClientRpc(new ClientRpcParams { Send = new ClientRpcSendParams { TargetClientIds = new List<ulong> { 1 } } });
+            //RandomNumber.Value = new MyCustomData
+            //{
+            //    _int = 10,
+            //    _bool = false,
+            //    message = "All your base are belong to us!"
+            //};
+        }
+
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            spawnedObjectTransform.GetComponent<NetworkObject>().Despawn(true);
+            Destroy(spawnedObjectTransform.gameObject);
         }
 
         Vector3 moveDir = Vector3.zero;
@@ -32,5 +71,16 @@ public class PlayerNetwork : NetworkBehaviour
 
         float moveSpeed = 5f;
         transform.position += moveDir * moveSpeed * Time.deltaTime;
+    }
+
+    [ServerRpc]
+    private void TestServerRpc(ServerRpcParams serverRpcParams)
+    {
+        Debug.Log("TestServerRpc " + OwnerClientId + "; " + serverRpcParams.Receive.SenderClientId);
+    }
+    [ClientRpc]
+    private void TestClientRpc(ClientRpcParams clientRpcParams)
+    {
+        Debug.Log("Test Client Rpc");
     }
 }
